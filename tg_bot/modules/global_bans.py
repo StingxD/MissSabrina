c06185e1f6db8df3bb5ec663aef3831e4f4fe26b
thakorrohan22 <48714583+thakorrohan22@users.noreply.github.com>
2020-06-07 12:59:11 +0530

    Update global_bans.py

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -388,7 +388,7 @@ UNGBAN_HANDLER = CommandHandler("ungban", ungban, pass_args=True,
 GBAN_LIST = CommandHandler("gbanlist", gbanlist,
                            filters=CustomFilters.sudo_filter | CustomFilters.support_filter)
 
-GBAN_STATUS = CommandHandler(gbanstat, pass_args=True, filters=Filters.group)
+GBAN_STATUS = CommandHandler("gbanstat" , pass_args=True, filters=Filters.group)
 
 GBAN_ENFORCER = MessageHandler(Filters.all & Filters.group, enforce_gban)
 
